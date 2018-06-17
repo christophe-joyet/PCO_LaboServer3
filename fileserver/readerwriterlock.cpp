d905a44af14bb681529d1a34aff23b8db49e5b75
Christophe Joyet <christophe.joyet@heig-vd.ch>
2018-06-17 12:56:31 +0200

    Implementation du moniteur de mesa par Marc dans classe ReaderWriterLock.h/.cpp

--- fileserver/readerwriterlock.cpp
+++ fileserver/readerwriterlock.cpp
@@ -1,4 +1,4 @@
-#include "readerwriterlock.h"
+/*#include "readerwriterlock.h"
 
 //Sémantique reprise du cours lecteur - redacteur priorité égale
 void ReaderWriterLock::lockReading() {
@@ -37,4 +37,69 @@ void ReaderWriterLock::unlockWriting() {
     fifo.release();
 
     mutex.release();
+}*/
+
+//Mesa
+#include "readerwriterlock.h"
+
+//Sémantique reprise du cours lecteur - redacteur priorité égale
+void ReaderWriterLock::lockReading() {
+    //fifo.acquire();
+    mutex.lock();
+    //nbReaders++;
+
+    while(nbWriters != 0){
+        waitNoWriters.wait(&mutex);
+    }
+    //writers.lock();
+    nbReaders++;
+
+    /*if (nbReaders==1){
+        writer.acquire();
+    }*/
+    mutex.unlock();
 }
+
+void ReaderWriterLock::unlockReading() {
+    mutex.lock();
+    nbReaders--;
+    // On réveil tous les rédacteur, au cas où il n'y
+    // aurait plus de rédacteur.
+    waitNoReaders.wakeAll();
+    /*if (nbReaders==0) {
+        writer.release();
+    }*/
+    mutex.unlock();
+}
+
+void ReaderWriterLock::lockWriting() {
+    mutex.lock();
+
+    while (nbWriters != 0 || nbReaders != 0) {
+        if(nbWriters != 0)
+            waitNoWriters.wait(&mutex);
+
+        if(nbReaders != 0)
+            waitNoReaders.wait(&mutex);
+    }
+
+    nbWriters++;
+
+    /*fifo.acquire();
+    writer.acquire();*/
+
+    mutex.unlock();
+}
+
+void ReaderWriterLock::unlockWriting() {
+    mutex.lock();
+
+
+    nbWriters--;
+    waitNoWriters.wakeAll();
+    /*writer.release();
+    fifo.release();*/
+
+    mutex.unlock();
+}
+
--- fileserver/readerwriterlock.h
+++ fileserver/readerwriterlock.h
@@ -1,3 +1,4 @@
+/*
 #ifndef READERWRITERLOCK_H
 #define READERWRITERLOCK_H
 
@@ -26,6 +27,59 @@ public:
 
     /**
      * @brief lockReading : Permet de bloquer la lecture dans la cache
+     /
+    void lockReading();
+
+    /**
+     * @brief lockWriting : Permet de bloquer l'écriture dans la cache
+     /
+    void lockWriting();
+    void unlockReading();
+    void unlockWriting();
+};
+
+#endif // READERWRITERLOCK_H
+*/
+
+//Mesa
+#ifndef READERWRITERLOCK_H
+#define READERWRITERLOCK_H
+
+#include <QSemaphore>
+#include <QWaitCondition>
+#include <QMutex>
+
+//Nous sommes partis sur une implémentation de priorité égale comme vu dans le cours
+
+class ReaderWriterLock
+{
+
+protected:
+    QMutex mutex; //pour garantir l'exclusion mutuelle
+    QSemaphore fifo;
+    QSemaphore writer;
+    bool isCacheFree;
+    QWaitCondition waitNoWriters;
+    QWaitCondition waitNoReaders;
+    QMutex readers;
+    QMutex writers;
+    int nbReaders;
+    int nbWriters;
+
+public:
+    ReaderWriterLock():
+        mutex(),
+        fifo(1),
+        writer(1),
+        nbReaders(0),
+        nbWriters(0),
+        waitNoReaders(),
+        waitNoWriters(),
+        readers(),
+        writers(){}
+
+    /**
+     * @brief lockReading : Permet de bloquer la lecture dans la cache
      */
     void lockReading();
 
@@ -33,8 +87,11 @@ public:
      * @brief lockWriting : Permet de bloquer l'écriture dans la cache
      */
     void lockWriting();
+
+
     void unlockReading();
     void unlockWriting();
 };
 
 #endif // READERWRITERLOCK_H
+
