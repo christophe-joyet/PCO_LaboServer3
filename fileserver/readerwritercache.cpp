4f3747c46d8fc922080c27d97f322e0af0371ce1
Christophe Joyet <christophe.joyet@heig-vd.ch>
2018-06-17 14:59:04 +0200

    Ajout des fonctions unlock et lock dans la classe ReaderWriterCache

--- fileserver/readerwritercache.cpp
+++ fileserver/readerwritercache.cpp
@@ -22,20 +22,25 @@ void ReaderWriterCache::putResponse(Response &response)
 {
     //création du timer correspondant à la réponses qui va être mise dans la cache
     TimestampedResponse timeStamped = {response, QDateTime::currentMSecsSinceEpoch()/1000};
+    lock.lockReading(); //blocage de la lecture du cache
     //Insertion de la réponse dans la cache
     map.insert(response.getRequest().getFilePath(), timeStamped);
+    lock.unlockReading();//libération de la lecture du cache
 }
 
 Option<Response> ReaderWriterCache::tryGetCachedResponse(Request &request) {
+    lock.lockWriting(); //on bloque l'accès au rédacteur au cache
     Option<Response> res = Option<Response>::none();
 
     auto elementInCache = map.find(request.getFilePath());
 
     //si on a trouvé l'élément dans la cache grace au filePath
     if(elementInCache != map.end()){
+        lock.unlockWriting(); //on débloque l'accès au rédacteur
         return res.some(elementInCache->response);
     }
 
+    lock.unlockWriting(); //on débloque l'accès au rédacteur
     //si on a rien trouvé dans la cache on retourne none()
     return res;
 }
